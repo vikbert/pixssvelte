0f6de680ccbe116504aedf52c22cafce5d671bd6
Xun Zhou <segentor@gmail.com>
2021-02-12 17:00:42 +0100

    updates

---
+++ src/components/Grid.svelte
@@ -0,0 +1,18 @@
+<div class="container">
+  <div class="centered-xy my-2" />
+  <div class="items">
+    <slot></slot>
+  </div>
+</div>
+
+<style>
+  .container {
+    max-width: 112rem !important;
+    height: 85vh;
+  }
+  .items {
+    display: grid;
+    grid-gap: 1.5rem;
+    grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  }
+</style>
